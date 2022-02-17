5d10e5e882a61370becee162a0741886d397063a
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-02-17 18:13:01 +0600

    new commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    //food section
+    const foodField = document.getElementById('food-input');
+    const foodInput = foodField.value;
+    const foodExpense = parseFloat(foodInput);
+
+    //rent section
+    const rentField = document.getElementById('rent-input');
+    const rentInput = rentField.value;
+    const rentExpense = parseFloat(rentInput);
+
+    //cloth section
+    const clothField = document.getElementById('cloth-input');
+    const clothInput = clothField.value;
+    const clothExpense = parseFloat(clothInput);
+
+    
+    //total expenses
+    const expenseField = document.getElementById('total-expense');
+    const previousExpenseText = expenseField.innerText;
+    const previousExpenseAmount = parseFloat(previousExpenseText);
+    const newExpenseAmount = foodExpense+rentExpense+clothExpense;
+
+    expenseField.innerText = newExpenseAmount; 
+
+    //income section
+
+    const incomeField = document.getElementById('income-input');
+    const incomeInput = incomeField.value;
+    const totalIncome = parseFloat(incomeInput);
+});
